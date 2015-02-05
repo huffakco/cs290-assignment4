a18a709b66978881f9c9b5b0ebeb70e51b276bdb
Corinna Huffaker <huffakco@onid.oregonstate.edu>
2015-02-04 22:04:17 -0800

    These are demo files to help learn about git functionality

--- helloworld.php
+++ helloworld.php
@@ -5,32 +5,77 @@
 </head>
 <body>
 <?php echo '<p>Hello World</p>'; 
-phpinfo(); 
-echo $_SERVER['HTTP_USER_AGENT'];
+?>
+
+<?php
+//Reference:
+//http://www.w3schools.com/php/php_functions.asp
+function writeMsg() {
+  echo "Hello world!";
+  echo "<br>";
+}
 ?>
 <?php
-
+writeMsg(); 
+?>
+<?php
+//Reference:
+//http://www.w3schools.com/php/php_looping_for.asp
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+} 
+?>
+<?php
 function foo()
 {
     echo '<p>Example function.\n</p>';
     return $retval;
 }
-?>
-<?php
+//Reference:
+//http://www.w3schools.com/php/php_forms.asp
+    echo '<br>';
+    echo 'Welcome  ';
+    echo $_GET["name"]; 
+    echo '<br>';
+    echo 'Your email address is:';
+    echo $_GET["email"]; 
+    echo '<br>';
+ 
+    echo '<br>';
+    echo 'Welcome  ';
+    echo $_POST["name"]; 
+    echo '<br>';
+    echo 'Your email address is:';
+    echo $_POST["email"]; 
+    echo '<br>';
+
+ 
+// Reference
+// PHP manual slightly modified
 function sum($numbers) {
     $acc = 0;
-    for ($i=0; $i<$numbers.length; $i++) {
+    echo $numbers[0];
+    for ($i=0; $i < count($numbers); $i++) {
         $acc += $numbers[i];
     }
     return $acc;
-
- return;
 }
-
+//output 10 when called
 echo '<br>';
-echo sum(1, 2, 3, 4);
+$myarray = array();
+for ($j=1; $j < 5; $j++) {
+  array_push($myarray,j);
+  echo $j;
+  echo '<br>';
+  }
+
+
+$var1 = count($myarray);
+echo '<br>count: ';
+echo $var1;
+echo sum($myarray);
 
 ?>
-<!-- output 10 when called above -->
+
 </body>
 </html>
--- multtable.js
+++ multtable.js
@@ -32,9 +32,119 @@ var chkParams = function(minVal,maxVal,str) {
     return ('Minimum ' + str + ' larger than maximum.');
   }
   return(0);
-}
+};
 
-window.onload() {
+var generateHeader = function(id,minVal,val) {
+  var tblHeader = document.createElement('thead');
+  var row = document.createElement('tr');
+
+  // First bland cell
+  var tblHead = document.createElement('th');
+  tblHead.innerHTML = '';
+  row.appendChild(tblHead);
+ 
+  // Define header elements
+  for (var i = minVal; i < minVal + val; i++)
+  {
+    var tblHeadElem = document.createElement('th');
+    tblHeadElem.innerHTML = i;
+    row.appendChild(tblHeadElem);
+  }
+  tblHeader.appendChild(row);
+  id.appendChild(tblHeader);
+};
+
+
+var generateBody = function(id,valParams,val) {
+
+  var tblBody = document.createElement('tbody');
+
+  // creating all cells
+  for (var i = valParams.min-multiplier; i < valParams.min-multiplier + val ; i++) {
+    // creates a table row
+    var row = document.createElement('tr');
+    // create first element in row
+    var cell1 = document.createElement('td');
+    cell1.innerHTML = i;
+    for (var j = valParams.min-multiplicand; j < valParams.min-multiplicand + val) {
+      var cell1 = document.createElement('td');
+      cell1.innerHTML = i * j;
+      row.appendChild(cell1);
+    }
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+  // add table body to table
+  id.appendChild(tblBody);
+};
+
+
+var generateTable = function() {
+/* Generate a table around testVal variable  */
+/* Reference: */
+/* https://developer.mozilla.org/en-US/docs/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces */
+
+  //Caluculate multiplicand and multiplier
+  var multiplicand = testVal.max-multiplicand - testVal.min-multiplicand + 2;
+  var multiplier = testVal.max-multiplier - testVal.min-multiplier + 2;
+
+  // creates a <table> element and a <tbody> element
+  var elementId = document.getElementById('putTableHere');
+  var tbl = document.createElement('table');
+ 
+  // generate table header
+  generateHeader(tbl,testVal.min-multiplicand,multiplicand);
   
+  // generate table body
+  generateBody(tbl,testVal,multiplier);
+  
+   // appends <table> into <body>
+  elementId.appendChild(tbl);
+};
 
+
+buildTable = function() {
+  var result = chkValid(tstVal.min-multiplicand,'min-multiplicand');
+  if ( result !== 0) {
+    console.log(result);
+  }
+  else if {
+    var result = chkValid(tstVal.max-multiplicand,'max-multiplicand');
+    if ( result !== 0) {
+      console.log(result);
+    }
+  }
+  else if {
+    var result = chkValid(tstVal.min-multiplier,'min-multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+  }
+  else if {
+    var result = chkValid(tstVal.max-multiplier,'max-multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+  }
+  else if {
+    var result = chkParams(tstVal.min-multiplicand,tstVal.max-multiplicand,'multiplicand');
+    if ( result !== 0) {
+      console.log(result);
+    }
+  }
+  else if {
+    var result = chkParams(tstVal.min-multiplier,tstVal.max-multiplier,'multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+  }
+  else {
+    generateTable();
+  }
+};
+
+
+window.onload = function() {
+  console.log('Start script');
+  buildTable();
 }
