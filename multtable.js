cf9f633b70fed1a0e1b6b585abe13257ffabf62a
Corinna Huffaker <huffakco@onid.oregonstate.edu>
2015-02-06 07:01:11 -0800

    Finish getting js version working as prototype

--- multtable.js
+++ multtable.js
@@ -1,30 +1,40 @@
 /* Javascript version of multtable.php */
 
 function MultiParams(params) {
-  this.min-multiplicand;
-  this.max-multiplicand;
-  this.min-multiplier;
-  this.max-multiplier;
+  this.min_multiplicand = params.min_multiplicand;
+  this.max_multiplicand = params.max_multiplicand;
+  this.min_multiplier = params.min_multiplier;
+  this.max_multiplier = params.max_multiplier;
   }
   
-var testVal = new MultiParams(
-      {min-multiplicand: 2,
-       max-multiplicand: 2,
-       min-multiplier: 3,
-       max-multiplier: 3
-      );
+var testVal = new MultiParams (
+      {min_multiplicand: 2,
+       max_multiplicand: 2,
+       min_multiplier: 3,
+       max_multiplier: 3 });
   
 var chkValid = function(val, str) {
     if (!(val)) {
         return ('Missing parameter ' + str);
     }
     else {
-      if (checker(val) < 0 {
+      if (checker(val) < 0) {
         return(str + ' not a number');
       }
     }
     return(0);
 };
+
+var checker = function(val)  {
+  if (typeof(val) === 'number')
+  {
+    return(val);
+  }
+  else {
+    return (-1);
+  }
+};
+
 
 var chkParams = function(minVal,maxVal,str) {
   if ((maxVal - minVal) < 0)
@@ -60,13 +70,14 @@ var generateBody = function(id,valParams,val) {
   var tblBody = document.createElement('tbody');
 
   // creating all cells
-  for (var i = valParams.min-multiplier; i < valParams.min-multiplier + val ; i++) {
+  for (var i = valParams.min_multiplier; i < valParams.min_multiplier + val ; i++) {
     // creates a table row
     var row = document.createElement('tr');
     // create first element in row
     var cell1 = document.createElement('td');
     cell1.innerHTML = i;
-    for (var j = valParams.min-multiplicand; j < valParams.min-multiplicand + val) {
+    row.appendChild(cell1);
+    for (var j = valParams.min_multiplicand; j < valParams.min_multiplicand + val; j++) {
       var cell1 = document.createElement('td');
       cell1.innerHTML = i * j;
       row.appendChild(cell1);
@@ -85,15 +96,15 @@ var generateTable = function() {
 /* https://developer.mozilla.org/en-US/docs/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces */
 
   //Caluculate multiplicand and multiplier
-  var multiplicand = testVal.max-multiplicand - testVal.min-multiplicand + 2;
-  var multiplier = testVal.max-multiplier - testVal.min-multiplier + 2;
+  var multiplicand = testVal.max_multiplicand - testVal.min_multiplicand + 2;
+  var multiplier = testVal.max_multiplier - testVal.min_multiplier + 2;
 
   // creates a <table> element and a <tbody> element
   var elementId = document.getElementById('putTableHere');
   var tbl = document.createElement('table');
  
   // generate table header
-  generateHeader(tbl,testVal.min-multiplicand,multiplicand);
+  generateHeader(tbl,testVal.min_multiplicand,multiplicand);
   
   // generate table body
   generateBody(tbl,testVal,multiplier);
@@ -104,42 +115,39 @@ var generateTable = function() {
 
 
 buildTable = function() {
-  var result = chkValid(tstVal.min-multiplicand,'min-multiplicand');
+  var result = chkValid(testVal.min_multiplicand,'min-multiplicand');
   if ( result !== 0) {
     console.log(result);
   }
-  else if {
-    var result = chkValid(tstVal.max-multiplicand,'max-multiplicand');
-    if ( result !== 0) {
-      console.log(result);
-    }
-  }
-  else if {
-    var result = chkValid(tstVal.min-multiplier,'min-multiplier');
-    if ( result !== 0) {
-      console.log(result);
-    }
-  }
-  else if {
-    var result = chkValid(tstVal.max-multiplier,'max-multiplier');
-    if ( result !== 0) {
-      console.log(result);
-    }
-  }
-  else if {
-    var result = chkParams(tstVal.min-multiplicand,tstVal.max-multiplicand,'multiplicand');
-    if ( result !== 0) {
-      console.log(result);
-    }
-  }
-  else if {
-    var result = chkParams(tstVal.min-multiplier,tstVal.max-multiplier,'multiplier');
-    if ( result !== 0) {
-      console.log(result);
-    }
-  }
   else {
+    var result = chkValid(testVal.max_multiplicand,'max-multiplicand');
+    if ( result !== 0) {
+      console.log(result);
+    }
+    else {
+    var result = chkValid(testVal.min_multiplier,'min-multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+    else {
+    var result = chkValid(testVal.max_multiplier,'max-multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+    else {
+    var result = chkParams(testVal.min_multiplicand,testVal.max_multiplicand,'multiplicand');
+    if ( result !== 0) {
+      console.log(result);
+    }
+    else {
+    var result = chkParams(testVal.min_multiplier,testVal.max_multiplier,'multiplier');
+    if ( result !== 0) {
+      console.log(result);
+    }
+    else {
     generateTable();
+    }
+    }}}}
   }
 };
 
