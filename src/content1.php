2911ddc52bb4a30e6c49bf389394d7cd79b0704c
Corinna Huffaker <huffakco@onid.oregonstate.edu>
2015-02-08 00:02:45 -0800

    Refactoring.  Links between content working.

--- src/content1.php
+++ src/content1.php
@@ -35,72 +35,85 @@ echo "\n<html>";
 echo "\n<head>";
 echo "\n<title>Content1</title>";
 // javascript with button function
-echo "\n<script type='text/javascript'>function logout() {";
+//echo "\n<script type='text/javascript'>function logout() {";
 //httpRequest = new XMLHttpRequest();
 //httpRequest.open('POST', 
 //'http://web.engr.oregonstate.edu/~huffakco/cs290-assignment4/content1.php',
 //false);
 //httpRequest.send(null);
-echo "};";
-echo "\n</script>";
+//echo "};";
+//echo "\n</script>";
 echo "\n</head>";
 echo "\n<body>";
 
-
-if(session_status() == PHP_SESSION_ACTIVE){
-  // check for user
-  echo "<br>Session is active<br>";
-  if (((!isset($_POST["username"])) || 
-      (empty($_POST["username"]))) &&
-      !($_SESSION['loginSuccess'])) {
-    echo "\nA username must be entered.  Click ";
-    echo "<a href=\"http://web.engr.oregonstate.edu/~huffakco/cs290-assignment4/login.php\">here</a> ";
-    echo "to return to the login screen.";
+function loginCheck() {
+  if (isset($_SESSION['loginSuccess']) && ($_SESSION['loginSuccess'])){
+    // do nothing logged in
   }
   else {
-    if ((!isset($_SESSION['username'])) || (empty($_SESSION['username']))) {
-      $_SESSION['username'] = $_POST['username'];
-      $_SESSION['loginSuccess'] = true;
+    if ((!isset($_POST["username"])) || 
+        (empty($_POST["username"])))  {
+      echo "\nA username must be entered.  Click ";
+      echo "<a href=\"http://web.engr.oregonstate.edu/~huffakco/cs290-assignment4/login.php\">here</a> ";
+      echo "to return to the login screen.";
+      $_SESSION['loginSuccess'] = false;
     }
     else {
-      if ($_SESSION['username'] != $_POST['username']) {
-        echo "\nA valid username must be entered.  Click ";
-        echo "<a href=\"http://web.engr.oregonstate.edu/~huffakco/cs290-assignment4/login.php\">here</a> ";
-        echo "to return to the login screen.";
-      }
-      else {
+      if ((!isset($_SESSION['username'])) || (empty($_SESSION['username']))) {
+        $_SESSION['username'] = $_POST['username'];
         $_SESSION['loginSuccess'] = true;
       }
-    }
-    if ($_SESSION['loginSuccess']) {
-      // Set the number of visits
-      if (!isset($_SESSION['visits'])) {
-        $_SESSION['visits'] = 0;
-        $n = $_SESSION['visits'];  
-        echo "<br>New visit: $n <br>";
-      }
       else {
-        $_SESSION['visits']++;
-        $n = $_SESSION['visits'];  
-        echo "<br>Return visit: $n<br>";
+        if ($_SESSION['username'] != $_POST['username']) {
+          echo "\nA valid username must be entered.  Click ";
+          echo "<a href=\"http://web.engr.oregonstate.edu/~huffakco/cs290-assignment4/login.php\">here</a> ";
+          echo "to return to the login screen.";
+          // login status doesn't change
+        }
+        else {
+          $_SESSION['loginSuccess'] = true;
+        }
       }
-      
-      $n = $_SESSION['visits'];  
-      echo '<br>Hello '.$_SESSION['username'].' you have visited this page ';
-      echo "$n times before. Click ";
-      echo "<a href=\"content1.php?logout=true\">here</a>";
-      echo " to logout.";
-      
-      // Display button to go to Content 2
- 
-      echo "\n<div><br>";
-      echo '<a href='.$url.'\content2.php?\'username\'=\''.$_SESSION['username'].'\',visits\'=\''.$_SESSION['visits'].'\'>Click for Content 2</a>';
-      echo "\n</div>";
- 
     }
-  }  
+  }
 }
 
+
+
+if(session_status() == PHP_SESSION_ACTIVE){
+  echo "<br>Session is active<br>";
+  // check login
+  loginCheck();
+  // logged in present content
+
+// if user logged in, display content
+  if ($_SESSION['loginSuccess']) {
+    // Set the number of visits
+    if (!isset($_SESSION['visits'])) {
+      $_SESSION['visits'] = 0;
+      $n = $_SESSION['visits'];  
+      echo "<br>New visit: $n <br>";
+    }
+    else {
+      $_SESSION['visits']++;
+      $n = $_SESSION['visits'];  
+      echo "<br>Return visit: $n<br>";
+    }
+    
+    $n = $_SESSION['visits'];  
+    echo '<br>Hello '.$_SESSION['username'].' you have visited this page ';
+    echo "$n times before. Click ";
+    echo "<a href=\"content1.php?logout=true\">here</a>";
+    echo " to logout.";
+    
+    // Display button to go to Content 2
+
+    echo "\n<div><br>";
+    echo '<a href='.$url.'\content2.php?\'username\'=\''.$_SESSION['username'].'\',visits\'=\''.$_SESSION['visits'].'\'>Click for Content 2</a>';
+    echo "\n</div>";
+
+  }  
+}
 
 //$username = $_POST["username"];
 //echo "\n<div>";
